9046630ec1078cb242f9cd55b39576f1c483244c
unknown <heetmehta208@gmail.com>
2024-07-04 23:17:46 +0530

    second comit

--- index.js
+++ index.js
@@ -90,7 +90,7 @@ io.on("connection", function (uniquesocket) {
     })
 })
 
-const PORT = process.env.PORT || 7000;
+const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
